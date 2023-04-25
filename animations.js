ec0da0b079d3e8e13892db02c6280f497c73244c
MercutioG <110576904+MercutioG@users.noreply.github.com>
2023-04-24 18:21:54 -0700

    Implimented the description part
    - Work in progress with accordion-like tabs

--- animations.js
+++ animations.js
@@ -2,13 +2,14 @@ $(window).on('load', function () {
   $('.info-btn').on('click', function(){
     let $infoBtn = $('.info-btn');
     $infoBtn.removeClass('active');
-    $changeBox = $(this).attr('rel')
+    $(this).addClass('active');
+    $changeBox = $(this).attr('rel');
     
-    $('.info-box.active').slideUp(250, changePanel)
+    $('.info-box.active').slideUp(300, changePanel)
 
     function changePanel(){
       $(this).removeClass('active');
-      $('#'+ $changeBox).slideDown(250, function(){
+      $('#'+ $changeBox).slideDown(300, function(){
         $(this).addClass('active');
       })
     }
